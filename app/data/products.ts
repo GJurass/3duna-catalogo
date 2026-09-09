3210da2e05a0110887b568b984cb44f0fae5e4e8
Gabriel de Lima Jurack <rimjurack@hotmail.com>
2026-09-09 14:44:54 -0300

    feat: adiciona carrossel de imagens à peça 011

--- app/data/products.ts
+++ app/data/products.ts
@@ -124,6 +124,14 @@ const productOverrides: Record<string, Partial<Product>> = {
       '/products/009-2.png'
     ]
   },
+  '011': {
+    image: '/products/011-2.png',
+    images: [
+      '/products/011-2.png',
+      '/products/011-1.png',
+      '/products/011-3.png'
+    ]
+  },
   '012': {
     image: '/products/012-1.png',
     images: [
